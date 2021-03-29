0d93d6aa5386063a20b04b5ec42b3e0ad7be41ce
cfiore-jpg <cameronfiore@Anthonys-MacBook-Air-2.local>
2021-03-28 23:15:36 -0400

    task class

---
+++ src/main/java/edu/brown/cs/student/weekli/schedule/Task.java
@@ -0,0 +1,72 @@
+package edu.brown.cs.student.weekli.schedule;
+
+import java.util.Calendar;
+
+public class Task implements Event {
+
+
+
+
+
+
+
+  /**
+   * Get the start date of the event.
+   *
+   * @return the start date
+   */
+  @Override
+  public Calendar getStartDate() {
+    return null;
+  }
+
+  /**
+   * Get the end date of the event.
+   *
+   * @return the end date
+   */
+  @Override
+  public Calendar getEndDate() {
+    return null;
+  }
+
+  /**
+   * Get the event description.
+   *
+   * @return the description
+   */
+  @Override
+  public String getDescription() {
+    return null;
+  }
+
+  /**
+   * Get the event category.
+   *
+   * @return the event category
+   */
+  @Override
+  public int getCategory() {
+    return 0;
+  }
+
+  /**
+   * Gets the name of the event.
+   *
+   * @return the name of the event
+   */
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  /**
+   * Get the estimated time to complete event.
+   *
+   * @return the estimated time
+   */
+  @Override
+  public long getEstimatedTime() {
+    return 0;
+  }
+}
