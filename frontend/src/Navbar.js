ddce43110ee7b983a491d2a889db62b4b7f55b6a
bubbledrift <60237318+bubbledrift@users.noreply.github.com>
2021-03-28 23:02:38 -0400

    added yellowness

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,23 @@
+import './Navbar.css';
+
+function Navbar() {
+
+
+    return (
+        <div className="flexbox-section">
+            <div className="flexbox-section">
+                <img src="./WeekliLogo.png" className="Logo"/>
+            </div>
+
+            <div className="flexbox-section">
+                April 2021
+            </div>
+
+            <div className="flexbox-section">
+                div3
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
