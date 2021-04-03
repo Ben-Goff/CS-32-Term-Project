fe2c620630dc6ff79963dd6c90e7b33cce4a2e36
bubbledrift <60237318+bubbledrift@users.noreply.github.com>
2021-04-03 17:45:58 -0400

    Prev and Next Weeks added

--- frontend/src/Calendar/CalendarContainer.js
+++ frontend/src/Calendar/CalendarContainer.js
@@ -2,18 +2,12 @@ import './CalendarContainer.css';
 import '../App.css';
 import Calendar from "./Calendar";
 
-function CalendarContainer() {
-    // Credit: https://stackoverflow.com/questions/4156434/javascript-get-the-first-day-of-the-week-from-current-date
-    function getMonday(d) {
-        d = new Date(d);
-        let day = d.getDay(),
-            diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
-        return new Date(d.setDate(diff));
-    }
+function CalendarContainer(props) {
+
 
     let dayNames = ["M", "T", "W", "Th", "F", "S", "Su"];
     let dayNumbers = [];
-    const monday = getMonday(new Date());
+    const monday = props.getDisplayMonday();
 
     let curDay = monday;
     for (let i = 0; i < 7; i++) {
--- frontend/src/TODO.md
+++ frontend/src/TODO.md
@@ -1,6 +1,8 @@
 ##TODO:
 
-Make the circle around the current day line up better
+Make the circle disappear when not on the current week.
+Change the month when going through weeks.
+Animations when going through weeks?
 Timezone indicator (?)
 Figure out different pages (login)
 Undo functionality
