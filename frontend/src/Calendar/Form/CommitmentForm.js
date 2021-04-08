b6af51c9c48910bd42d65db299b4d140f9838c8f
DustInMyGit <dustin.h.wu@gmail.com>
2021-04-08 18:25:49 -0400

    improved event-creating forms

--- frontend/src/Calendar/Form/CommitmentForm.js
+++ frontend/src/Calendar/Form/CommitmentForm.js
@@ -10,9 +10,13 @@ function CommitmentForm() {
                 <label htmlFor="description">Description</label><br/>
                 <input type="text" id="description" name="description"/><br/>
                 <label htmlFor="start-date">Start Date</label><br/>
-                <input type="text" id="start-date" name="start-date"/><br/>
-                <label htmlFor="start-date">End Date</label><br/>
-                <input type="text" id="end-date" name="end-date"/><br/>
+                <input type="date" id="start-date" name="start-date" placeholder={"mm-dd-yyyy"}/><br/>
+                <label htmlFor="start-time">Start Time</label><br/>
+                <input type="time" id="start-time" name="start-time" placeholder={"__:__ AM/PM"}/><br/>
+                <label htmlFor="end-date">End Date</label><br/>
+                <input type="date" id="end-date" name="end-date" placeholder={"mm-dd-yyyy"}/><br/>
+                <label htmlFor="end-time">End Time</label><br/>
+                <input type="time" id="end-time" name="end-time" placeholder={"__:__ AM/PM"}/><br/>
                 <input id="submit" type="submit" value="Create"/>
             </form>
         </div>
