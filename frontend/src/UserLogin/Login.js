405a4f2124dbad54e39e726a0e4f6aed4c59cbb3
bubbledrift <60237318+bubbledrift@users.noreply.github.com>
2021-04-08 17:24:51 -0400

    updated login and signup pages

--- frontend/src/UserLogin/Login.js
+++ frontend/src/UserLogin/Login.js
@@ -1,15 +1,70 @@
 import '../App.css';
 import './UserLogin.css';
 import { Link } from "react-router-dom";
+import {useState} from "react";
 
 function Login() {
 
+    //TODO: error handling when login is not valid.
+
+    let username = ""
+    let password = ""
+
+    const [showError, setShowError] = useState(false)
+
+    function changeUsername(event) {
+        username = event.target.value;
+    }
+
+    function changePassword(event) {
+        password = event.target.value;
+    }
+
+    function submitSignIn() {
+        console.log("user: " + username + " pass: " + password)
+    }
+
     return (
-        <Link to="/signup">
-            <a className="textLink">
-                Signup Here
-            </a>
-        </Link>
+        <div>
+            <img src="WeekliLogo.png" className="logo"/>
+            <h1>Weekli</h1>
+
+            <div>
+
+                <label htmlFor="username">Username</label><br/>
+                <input type="text" id="username" name="username" onChange={changeUsername}/><br/>
+                <label htmlFor="password">Password</label><br/>
+                <input type="text" id="password" name="password" onChange={changePassword}/><br/>
+
+                {/*Links to main page only if there is no error*/}
+                {showError &&
+                <div>
+                    <button onClick={submitSignIn}>Login</button>
+                    <h2>
+                        ERROR
+                    </h2>
+                </div>
+                }
+
+                {!showError &&
+                <div>
+                    <Link to="/">
+                        <button onClick={submitSignIn}>Login</button>
+                    </Link>
+                </div>
+                }
+
+
+            </div>
+
+
+            New User?
+            <Link to="/signup">
+                <a className="textLink">
+                    Signup Here
+                </a>
+            </Link>
+        </div>
     );
 }
 
--- frontend/src/UserLogin/Signup.js
+++ frontend/src/UserLogin/Signup.js
@@ -1,15 +1,85 @@
 import '../App.css';
 import './UserLogin.css';
 import { Link } from "react-router-dom";
+import {useState} from "react";
 
 function Signup() {
 
+    let username = ""
+    let password = ""
+    let confirm = ""
+
+    const [showError, setShowError] = useState(false)
+
+    function submitSignIn() {
+        console.log("user: " + username + " pass: " + password + " confirm: " + confirm)
+    }
+
+    function changeUsername(event) {
+        username = event.target.value;
+    }
+
+    function changePassword(event) {
+        password = event.target.value;
+        if (password !== confirm) {
+            setShowError(true)
+        } else {
+            setShowError(false)
+        }
+    }
+
+    function changeConfirm(event) {
+        confirm = event.target.value;
+        if (password !== confirm) {
+            setShowError(true)
+        } else {
+            setShowError(false)
+        }
+    }
+
     return (
-        <Link to="/login">
-            <a className="textLink">
-                Login Here
-            </a>
-        </Link>
+        <div>
+            <img src="WeekliLogo.png" className="logo"/>
+            <h1>Welcome to Weekli</h1>
+
+            <div>
+
+                <label htmlFor="username">Username</label><br/>
+                <input type="text" id="username" name="username" onChange={changeUsername}/><br/>
+                <label htmlFor="password">Password</label><br/>
+                <input type="text" id="password" name="password" onChange={changePassword}/><br/>
+                <label htmlFor="confirm">Confirm Password</label><br/>
+                <input type="text" id="confirm" name="confirm" onChange={changeConfirm}/><br/>
+
+                {/*Links to main page only if there is no error*/}
+                {showError &&
+                    <div>
+                        <button onClick={submitSignIn}>Sign Up</button>
+                        <h2>
+                            Error, password does not match confirm password.
+                        </h2>
+                    </div>
+                }
+
+                {!showError &&
+                <div>
+                    <Link to="/">
+                        <button onClick={submitSignIn}>Sign Up</button>
+                    </Link>
+                </div>
+                }
+
+
+            </div>
+
+            Returning User?
+            <Link to="/login">
+                <a className="textLink">
+                    Login Here
+                </a>
+            </Link>
+        </div>
+
     );
 }
 
