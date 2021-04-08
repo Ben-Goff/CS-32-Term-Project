5b299f873a3ee9aa0c50422e5b434ec3191fbf87
cfiore-jpg <cameronfiore@Anthonys-MacBook-Air-2.local>
2021-04-07 22:04:33 -0400

    before merge

--- src/main/java/edu/brown/cs/student/weekli/TriggerAction.java
+++
@@ -1,9 +0,0 @@
-package edu.brown.cs.student.weekli;
-
-/** Interface for all classes that execute a particular action given
- * the correct command and the correct number of arguments. */
-public interface TriggerAction {
-  String command();
-  String execute(String[] args, boolean isREPL);
-  int[] getNumParameters();
-}
